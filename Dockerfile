8141adda88b91bf0af8206357fd46df5f7f29817
militarpancho <militarpancho_93@msn.com>
2020-04-21 02:27:00 +0100

    add subtitles download

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,9 @@
 FROM python:3.7-slim
 
-WORKDIR /src
+WORKDIR /src/plexMetadataUpdater
 
 RUN apt-get update \
 	&& apt-get -y install git procps \
 	&& git clone https://github.com/emericg/OpenSubtitlesDownload.git
+
+COPY . /src/plexMetadataUpdater
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import re
-
+import subprocess
+import time
 def watch_file( dirs, pattern, check_interval=60 ):
     '''Return true if filename exists, if not keep checking once every check_interval seconds for time_limit seconds.
     time_limit defaults to 1 hour
@@ -8,20 +9,23 @@ def watch_file( dirs, pattern, check_interval=60 ):
     '''
 
     while True:
-
         for dir in dirs:
             for file in os.listdir(dir):
                 p = pattern.match(file)
-                if p and p.group('ext') != 'part':
-                    os.rename(os.path.join(dir,file), os.path.join(dir,"{} ({}).{}".format(" ".join(p.group('name').split(".")), p.group('year'), p.group('ext'))))
-
+                if p and (p.group('ext') in ['mp4', 'mkv', 'avi', 'mov']):
+                    print("finding subtitles for {}".format(os.path.join(dir,file)))
+                    subprocess.call(["python", "OpenSubtitlesDownload/OpenSubtitlesDownload.py", "--cli", "-i", "-a", "-l", "spa", os.path.join(dir,file)])
+                    f = file.split(".")
+                    new_f = "{} ({}).{}".format(" ".join(p.group('name').split(".")), p.group('year'), p.group('ext'))
+                    new_srt = "{} ({}).spa.srt".format(" ".join(p.group('name').split(".")), p.group('year'))
+                    os.rename(os.path.join(dir, ".".join(f[:-1])+"_es.srt"), os.path.join(dir, new_srt))
+                    os.rename(os.path.join(dir,file), os.path.join(dir, new_f))
         # Wait for check interval seconds, then check again.
         time.sleep( check_interval )
-
 
 if __name__ == '__main__':
     path = "/media/films"
     dirs = [f.path for f in os.scandir(path) if f.is_dir()]
     pattern = re.compile('(?P<name>.+)\.(?P<year>[0-9]+)\..*\.(?P<ext>.+$)')
-    check_interval=3600
+    check_interval=3600*10
     watch_file( dirs, pattern, check_interval )
